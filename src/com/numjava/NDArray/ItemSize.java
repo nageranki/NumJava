dab37940f571d18c599a682b602b0ad02ae200b9
Nagesh Eranki <nagesheranki04@gmail.com>
2024-11-02 15:27:25 +0530

    Revert "test3"
    This reverts commit d83bf7ca48215a50f39012b7fda442e7fa6723ae.

--- src/com/numjava/NDArray/ItemSize.java
+++
@@ -1,8 +0,0 @@
-package com.numjava.NDArray;
-
-// NumJava equivalent of numpy.ndarray.itemsize
-public class ItemSize {
-    public static int itemsize(NDArray array) {
-        return Double.BYTES; // Since NDArray stores data as double, each element takes 8 bytes
-    }
-}
